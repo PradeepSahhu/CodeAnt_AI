1d8db895366c98b50807f96b620db874db6a20df
Pradeep Sahu <official.pradeepsahu@gmail.com>
2024-12-22 22:47:49 +0530

    completed the repository section

--- app/Repository/page.js
+++ app/Repository/page.js
@@ -27,7 +27,7 @@ const NavItem = ({ imgSrc, message, selected }) => {
 
 const RepositoryDashboard = () => {
   return (
-    <div className="grid grid-cols-12  bg-white text-[#181D27]">
+    <div className="grid grid-cols-12  bg-[#E9EAEB] text-[#181D27]">
       {/* left navigation bar */}
       <div className="h-screen col-span-2 bg-white border-r-[1px] border-[#E9EAEB]">
         <nav className="flex flex-col gap-1 h-full justify-between">
@@ -76,17 +76,43 @@ const RepositoryDashboard = () => {
           </div>
         </nav>
       </div>
-      <div className="bg-white col-span-10 font-[inter]">
-        <div className=" border-[5px] border-[#E9EAEB]  ">
-          <div className="p-5 rounded-2xl">
-            <p className="font-[600] w-[146px] h-[32px]  text-[#181D27]">
-              Repositories
-            </p>
-            <p>33 total repositories</p>
+      <div className="bg-white col-span-10 font-[inter] h-screen">
+        <div className=" border-[5px] border-[#E9EAEB] my-2 py-3">
+          <div className="flex justify-between mx-2 rounded-2xl">
+            <div className="pb-2 px-1 ">
+              <p className="font-[600] text-2xl  text-[#181D27]">
+                Repositories
+              </p>
+              <p className="text-sm leading-[20px] font-extralight ">
+                33 total repositories
+              </p>
+            </div>
+            <div className="flex items-center gap-3 mx-2">
+              <button className="bg-white text-black px-3 py-2 rounded-md border-[1px] border-[#E9EAEB] flex items-center gap-x-3">
+                <img
+                  className="w-[20px] h-[20px]"
+                  src="./Repository/Frame_2.png"
+                />
+                Refresh All
+              </button>
+              <button className="bg-blue-600 text-white px-5 py-2 rounded-md flex items-center gap-x-3">
+                <img
+                  className="w-[20px] h-[20px]"
+                  src="./Repository/plus.png"
+                />
+                <p>Add Repository</p>
+              </button>
+            </div>
           </div>
-          <div></div>
-          <div>
-            <input type="search" />
+          <div className="mx-3">
+            <button className="flex items-center border-[1px] border-[#E(EAEB)] p-1 rounded-lg ">
+              <img className="px-2" src="./Repository/search.png" />
+              <input
+                className="px-3 py-1 pr-24 focus:outline-none "
+                type="search"
+                placeholder="Search Repositories"
+              />
+            </button>
           </div>
         </div>
       </div>
